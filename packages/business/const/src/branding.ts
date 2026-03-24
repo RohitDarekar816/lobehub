235629e903ffd989538274f3a94dc54f4250203f
Rohit Darekar <rohitdarekar816@gmail.com>
2026-03-24 08:27:31 +0530

    refactor: branding enforcement

--- packages/business/const/src/branding.ts
+++ packages/business/const/src/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeHub Cloud';
+export const LOBE_CHAT_CLOUD = 'Afintrix Cloud';
 
-export const BRANDING_NAME = 'LobeHub';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'Afintrix';
+export const BRANDING_LOGO_URL = '/logo-afintrix.svg';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'Afintrix';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -18,11 +18,11 @@ export const BRANDING_URL = {
 };
 
 export const SOCIAL_URL = {
-  discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+  discord: undefined,
+  github: undefined,
+  medium: undefined,
+  x: undefined,
+  youtube: undefined,
 };
 
 export const FILE_URL = {
@@ -30,11 +30,11 @@ export const FILE_URL = {
 };
 
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'hello@afintrix.com',
+  support: 'support@afintrix.com',
 };
 
-export const BRANDING_PROVIDER = 'lobehub';
+export const BRANDING_PROVIDER = 'afintrix';
 
 export const COPYRIGHT = `© ${new Date().getFullYear()} ${ORG_NAME}`;
 export const COPYRIGHT_FULL = `${COPYRIGHT}. All rights reserved.`;
